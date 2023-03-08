7d3631c2a5ad7fca0d7285d47bca5315550785e5
muhammadsaddamnur <muhammadsaddamnur@gmail.com>
2023-03-08 21:27:24 +0700

    perf: :zap: add more control (autoplay, loop, hover, controls)

--- lib/lottie_player.dart
+++ lib/lottie_player.dart
@@ -14,11 +14,27 @@ class LottiePlayer extends StatelessWidget {
   /// * [double] Creates a height for image
   final double? height;
 
+  /// * [bool] Creates a autoPlay for image
+  final bool? autoPlay;
+
+  /// * [bool] Creates a loop for image
+  final bool? loop;
+
+  /// * [bool] Creates a hover for image
+  final bool? hover;
+
+  /// * [bool] Creates a controls for image
+  final bool? controls;
+
   const LottiePlayer({
     Key? key,
     required this.networkUrl,
     this.width,
     this.height,
+    this.autoPlay = true,
+    this.loop = true,
+    this.hover = true,
+    this.controls = true,
   }) : super(key: key);
 
   @override
@@ -27,7 +43,7 @@ class LottiePlayer extends StatelessWidget {
     double mHeight = MediaQuery.of(context).size.width;
     return WebViewX(
       initialContent:
-          '<script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script> <lottie-player style=".scrollbar-auto{scrollbar-color:auto;overflow-y:scroll;}" mode="normal" src="$networkUrl"  background="transparent"  speed="1"  style="width: ${width ?? mWidth}px; height: ${height ?? mHeight}px;"  loop autoplay></lottie-player>',
+          '<script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script> <lottie-player style=".scrollbar-auto{scrollbar-color:auto;overflow-y:scroll;}" mode="normal" src="$networkUrl"  background="transparent"  speed="1"  style="width: ${width ?? mWidth}px; height: ${height ?? mHeight}px;" ${hover == false ? '' : 'hover'} ${loop == false ? '' : 'loop'} ${controls == false ? '' : 'controls'} ${autoPlay == false ? '' : 'autoplay'}></lottie-player>',
       initialSourceType: SourceType.html,
       width: width ?? mWidth,
       height: height ?? mHeight,
